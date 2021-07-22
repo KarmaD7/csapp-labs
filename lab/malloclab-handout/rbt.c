f51f531a518764f09494cb6709dc107c79ebd3f2
丁韶峰 <thudsf19@gmail.com>
2021-07-22 23:54:44 +0800

    malloc lab: red-black tree framework

---
+++ lab/malloclab-handout/rbt.c
@@ -0,0 +1,41 @@
+#include "stdio.h"
+
+struct node {
+  /* data */
+  int* len;
+  int color;
+  struct node* lchild;
+  struct node* rchild;
+};
+
+struct node* root;
+void search();
+void insert();
+void delete();
+void solve_double_black();
+void solve_double_red();
+
+int main() {
+  ;
+  return 0;
+}
+
+void search() {
+  ;
+}
+
+void insert() {
+  ;
+}
+
+void delete() {
+  ;
+}
+
+void solve_double_red() {
+  ;
+}
+
+void solve_double_black() {
+  ;
+}
