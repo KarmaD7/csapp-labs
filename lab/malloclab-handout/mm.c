3eedce16a350f770335a3b05debe0e0860493201
丁韶峰 <thudsf19@gmail.com>
2021-08-16 23:03:06 +0800

    basic malloc framework (copy from book)

--- lab/malloclab-handout/mm.c
+++ lab/malloclab-handout/mm.c
@@ -44,12 +44,58 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1 << 12)
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+#define PACK(size, alloc) ((size) | (alloc))
+
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE((char *)(bp) - WSIZE))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE))
+
+static char* heap_listp;
+static void* extend_heap(size_t words);
+static void* coalesce(void *bp);
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1)
+        return -1;
+    PUT(heap_listp, 0);
+    PUT(heap_listp + WSIZE, PACK(DSIZE, 1));
+    PUT(heap_listp + 2 * WSIZE, PACK(DSIZE, 1));
+    PUT(heap_listp + 3 * WSIZE, PACK(0, 1));
+    heap_listp += 2 * WSIZE;
     return 0;
+}
+
+static void* extend_heap(size_t words) {
+    char* bp;
+    size_t size;
+
+    size = (words % 2) ? (words + 1) * WSIZE: words * WSIZE;
+    if ((long)((bp = mem_sbrk(size))) == -1)
+        return NULL;
+    
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+    return coalesce(bp);
 }
 
 /* 
@@ -73,8 +119,35 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    size_t size = GET_SIZE(ptr);
+    PUT(HDRP(ptr), PACK(size, 0));
+    PUT(FTRP(ptr), PACK(size, 0));
+    coalesce(ptr);
 }
 
+static void* coalesce(void *bp) {
+    size_t size = GET_SIZE(bp);
+    size_t last_free = GET_ALLOC(PREV_BLKP(bp));
+    size_t next_free = GET_ALLOC(NEXT_BLKP(bp));
+
+    if (last_free && next_free) {
+        ;
+    } else if (last_free && !next_free) {
+
+    } else if (!last_free && next_free) {
+
+    } 
+
+    return bp;
+}
+
+static void* find_fit(size_t asize) {
+    ;
+}
+
+static void place(void* bp, size_t asize) {
+    ;
+}
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
  */
